e12a12c2e0d1b562da1c21ccaafc36c4e1139aff
unknown <andejt1@student.op.ac.nz>
2020-06-08 16:13:14 +1200

    ...

--- app.py
+++ app.py
@@ -37,6 +37,33 @@ def loadData():
 	
 @app.route('/')
 def index():
+	for file in os.listdir(app.config['FILES_FOLDER']):
+    filename = os.fsdecode(file)
+    path = os.path.join(app.config['FILES_FOLDER'],data1.csv)
+    f = open(path)
+    r = csv.DictReader(f) 
+    d = list(r)
+    for data in d:
+        country = Country() # a blank placeholder country
+        dict = {} # a blank placeholder data dict
+        for key in data: # iterate through the header keys
+            if key == "country":
+                # check if this country already exists in the db
+                
+                # if the country does not exist, we can use the new blank country we created above, and set the name
+
+                # if the country already exists, replace the blank country with the existing country from the db, and replace the blank dict with the current country's 
+                # data                
+            else:
+                f = filename.replace(".csv","") # we want to trim off the ".csv" as we can't save anything with a "." as a mongodb field name
+                if f in dict: # check if this filename is already a field in the dict
+                    dict[f][key] = data[key] # if it is, just add a new subfield which is key : data[key] (value)
+                else:
+                    dict[f] = {key:data[key]} # if it is not, create a new object and assign it to the dict
+
+            # add the data dict to the country
+
+        # save the country
 	return render_template('index.html'), 200
 	
 @app.route('/inspiration')
