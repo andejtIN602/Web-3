a0555540f2bb7b23f01fba90bd8e4b8dd994c919
Jamie Anderson (1000023176) <ANDEJT1@student.op.ac.nz>
2020-06-16 12:05:54 +1200

    ...

--- myapp2/routes/users.js
+++ myapp2/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controllers/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', userController.index);
+router.get('/add', userController.add);
+router.get('/delete', userController.delete);
+router.get('/edit', userController.edit);
 
 module.exports = router;
+module.exports = {
+    index(req, res){
+      return res.status(200).send('respond with a resource');
+    }
+  }
